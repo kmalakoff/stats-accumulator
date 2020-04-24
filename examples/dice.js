f0f17444e8fc7c83a9cf3fbaff13db91ad7ed8fe
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-23 21:53:37 -0700

    Format and rename index.js

--- examples/dice.js
+++ examples/dice.js
@@ -1,11 +1,11 @@
-var dice = require('dice')
+var dice = require('dice');
 
-var Stats = require('../stats')
-var s = Stats()
+var Stats = require('../stats');
+var s = Stats();
 
 for (var i = 0; i < 100; i++) {
-  s.update(dice.sum(dice.roll('2d6')))
-  console.log(s.toJSON())
+  s.update(dice.sum(dice.roll('2d6')));
+  console.log(s.toJSON());
 }
 
-console.log(s)
+console.log(s);
